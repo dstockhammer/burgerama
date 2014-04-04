c273e5ccde606639e8b1a21bae27441970246d13
Daniel Stockhammer <daniel.stockhammer@dfconline.com>
2014-04-04 15:30:36 +0100

    Fleshed out the domain for venues.

--- Services/Venues/Core/Venue.cs
+++ Services/Venues/Core/Venue.cs
@@ -1,12 +1,51 @@
-﻿namespace Burgerama.Services.Venues.Core
+﻿using System;
+
+namespace Burgerama.Services.Venues.Core
 {
     public sealed class Venue
     {
+        public Guid Id { get; private set; }
+
         public string Title { get; private set; }
+
+        public int Votes
+        {
+            get { return 0; }
+        }
+
+        public double Rating
+        {
+            get { return 0; }
+        }
+
+        public Venue(Guid id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
 
         public Venue(string title)
         {
+            Id = Guid.NewGuid();
             Title = title;
+        }
+
+        public void AddVote(Guid user)
+        {
+            // todo: only allow to vote if there is no outing with this venue
+
+            var vote = new Vote(user);
+
+            // todo: do something to actually add the vote
+        }
+
+        public void AddRating(Guid user, int value)
+        {
+            // todo: only allow to rate if the user has attendend an outing with this venue
+
+            var rating = new Rating(user, value);
+
+            // todo: do something to actually add the rating
         }
     }
 }
