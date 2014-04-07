2570e697fd9b6beb10f2d757b142f3c429d3ad5c
zsimari <zoltan.simari@memcf.co.uk>
2014-04-07 19:11:29 +0100

    Adding typing and Google maps trial

---
+++ Web/UI/Default.js
@@ -0,0 +1,77 @@
+﻿function initialize() {
+    var mapOptions = {
+        center: new google.maps.LatLng(-34.397, 150.644),
+        overviewMapControl: false,
+        scaleControl: false,
+        streetViewControl: false,
+        zoomControl: true,
+        zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.LARGE,
+            position: google.maps.ControlPosition.RIGHT_CENTER
+        },
+        mapTypeControl: false,
+        panControl: false,
+        zoom: 12
+    };
+
+    var map = new google.maps.Map(document.getElementById("map-canvas"),
+        mapOptions);
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            map.setCenter(initialLocation);
+            map.setZoom(mapOptions.zoom);
+        });
+    }
+
+    var input = document.getElementById('central-search-box');
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    var searchBox = new google.maps.places.SearchBox(input);
+
+    // Listen for the event fired when the user selects an item from the
+    // pick list. Retrieve the matching places for that item.
+    google.maps.event.addListener(searchBox, 'places_changed', function () {
+        var places = searchBox.getPlaces();
+
+        for (var i = 0, marker; marker = markers[i]; i++) {
+            marker.setMap(null);
+        }
+
+        // For each place, get the icon, place name, and location.
+        markers = [];
+        var bounds = new google.maps.LatLngBounds();
+        for (var i = 0, place; place = places[i]; i++) {
+            var image = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            var marker = new google.maps.Marker({
+                map: map,
+                icon: image,
+                title: place.name,
+                position: place.geometry.location
+            });
+
+            markers.push(marker);
+
+            bounds.extend(place.geometry.location);
+        }
+
+        map.fitBounds(bounds);
+    });
+
+    // Bias the SearchBox results towards places that are within the bounds of the
+    // current map's viewport.
+    google.maps.event.addListener(map, 'bounds_changed', function () {
+        var bounds = map.getBounds();
+        searchBox.setBounds(bounds);
+    });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
---
+++ Web/UI/Scripts/typings/angularjs/angular-resource-tests.js
@@ -0,0 +1,123 @@
+/// <reference path="angular-resource.d.ts" />
+;
+;
+
+///////////////////////////////////////
+// IActionDescriptor
+///////////////////////////////////////
+var actionDescriptor;
+
+actionDescriptor.headers = { header: 'value' };
+actionDescriptor.isArray = true;
+actionDescriptor.method = 'method action';
+actionDescriptor.params = { key: 'value' };
+
+///////////////////////////////////////
+// IResourceClass
+///////////////////////////////////////
+var resourceClass;
+var resource;
+var resourceArray;
+
+resource = resourceClass.delete();
+resource = resourceClass.delete({ key: 'value' });
+resource = resourceClass.delete({ key: 'value' }, function () {
+});
+resource = resourceClass.delete(function () {
+});
+resource = resourceClass.delete(function () {
+}, function () {
+});
+resource = resourceClass.delete({ key: 'value' }, { key: 'value' });
+resource = resourceClass.delete({ key: 'value' }, { key: 'value' }, function () {
+});
+resource = resourceClass.delete({ key: 'value' }, { key: 'value' }, function () {
+}, function () {
+});
+
+resource = resourceClass.get();
+resource = resourceClass.get({ key: 'value' });
+resource = resourceClass.get({ key: 'value' }, function () {
+});
+resource = resourceClass.get(function () {
+});
+resource = resourceClass.get(function () {
+}, function () {
+});
+resource = resourceClass.get({ key: 'value' }, { key: 'value' });
+resource = resourceClass.get({ key: 'value' }, { key: 'value' }, function () {
+});
+resource = resourceClass.get({ key: 'value' }, { key: 'value' }, function () {
+}, function () {
+});
+
+resourceArray = resourceClass.query();
+resourceArray = resourceClass.query({ key: 'value' });
+resourceArray = resourceClass.query({ key: 'value' }, function () {
+});
+resourceArray = resourceClass.query(function () {
+});
+resourceArray = resourceClass.query(function () {
+}, function () {
+});
+resourceArray = resourceClass.query({ key: 'value' }, { key: 'value' });
+resourceArray = resourceClass.query({ key: 'value' }, { key: 'value' }, function () {
+});
+resourceArray = resourceClass.query({ key: 'value' }, { key: 'value' }, function () {
+}, function () {
+});
+
+resource = resourceClass.remove();
+resource = resourceClass.remove({ key: 'value' });
+resource = resourceClass.remove({ key: 'value' }, function () {
+});
+resource = resourceClass.remove(function () {
+});
+resource = resourceClass.remove(function () {
+}, function () {
+});
+resource = resourceClass.remove({ key: 'value' }, { key: 'value' });
+resource = resourceClass.remove({ key: 'value' }, { key: 'value' }, function () {
+});
+resource = resourceClass.remove({ key: 'value' }, { key: 'value' }, function () {
+}, function () {
+});
+
+resource = resourceClass.save();
+resource = resourceClass.save({ key: 'value' });
+resource = resourceClass.save({ key: 'value' }, function () {
+});
+resource = resourceClass.save(function () {
+});
+resource = resourceClass.save(function () {
+}, function () {
+});
+resource = resourceClass.save({ key: 'value' }, { key: 'value' });
+resource = resourceClass.save({ key: 'value' }, { key: 'value' }, function () {
+});
+resource = resourceClass.save({ key: 'value' }, { key: 'value' }, function () {
+}, function () {
+});
+
+///////////////////////////////////////
+// IResourceService
+///////////////////////////////////////
+var resourceService;
+resourceClass = resourceService('test');
+resourceClass = resourceService('test');
+resourceClass = resourceService('test');
+
+///////////////////////////////////////
+// IModule
+///////////////////////////////////////
+var mod;
+var resourceServiceFactoryFunction;
+var resourceService;
+
+resourceClass = resourceServiceFactoryFunction(resourceService);
+
+resourceServiceFactoryFunction = function (resourceService) {
+    return resourceClass;
+};
+mod = mod.factory('factory name', resourceServiceFactoryFunction);
+//# sourceMappingURL=angular-resource-tests.js.map
