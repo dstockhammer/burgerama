dff2ae80a604ce35c3b5c2d419ccdf635c573efa
zsimari <zoltan.simari@memcf.co.uk>
2014-04-08 18:52:19 +0100

    UI Changes: adding map control

--- Web/UI/Default.js
+++ Web/UI/Default.js
@@ -11,7 +11,7 @@
         },
         mapTypeControl: false,
         panControl: false,
-        zoom: 12
+        zoom: 15
     };
 
     var map = new google.maps.Map(document.getElementById("map-canvas"),
@@ -28,18 +28,19 @@
     var input = document.getElementById('central-search-box');
     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
     var searchBox = new google.maps.places.SearchBox(input);
+    var markers = [];
 
     // Listen for the event fired when the user selects an item from the
     // pick list. Retrieve the matching places for that item.
     google.maps.event.addListener(searchBox, 'places_changed', function () {
         var places = searchBox.getPlaces();
 
+        
         for (var i = 0, marker; marker = markers[i]; i++) {
             marker.setMap(null);
         }
 
         // For each place, get the icon, place name, and location.
-        markers = [];
         var bounds = new google.maps.LatLngBounds();
         for (var i = 0, place; place = places[i]; i++) {
             var image = {
@@ -55,7 +56,8 @@
                 map: map,
                 icon: image,
                 title: place.name,
-                position: place.geometry.location
+                position: place.geometry.location,
+                animation: google.maps.Animation.DROP
             });
 
             markers.push(marker);
@@ -64,13 +66,14 @@
         }
 
         map.fitBounds(bounds);
+        map.setZoom(mapOptions.zoom);
     });
 
     // Bias the SearchBox results towards places that are within the bounds of the
     // current map's viewport.
     google.maps.event.addListener(map, 'bounds_changed', function () {
         var bounds = map.getBounds();
-        searchBox.setBounds(bounds);
+        searchBox.setBounds(bounds);    
     });
 }
 
