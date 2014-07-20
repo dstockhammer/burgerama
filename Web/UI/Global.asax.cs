171d8aee85e1ae5b1810de177fde297e1bc318ce
Zoltan Simari <simari.zoltan@gmail.com>
2014-07-20 22:24:37 +0100

    #31: Adding configurable redirect mechanism for HTTP -> HTTPS

--- Web/UI/Global.asax.cs
+++ Web/UI/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System.Configuration;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -7,11 +8,25 @@ namespace Burgerama.Web.UI
 {
     public class MvcApplication : HttpApplication
     {
+        private readonly bool isHttpsRedirectionEnabled;
+
+        public MvcApplication()
+        {
+            var securityRedirectionConfig = ConfigurationManager.AppSettings["security:isHttpsRedirectionEnabled"];
+            isHttpsRedirectionEnabled = securityRedirectionConfig != null && securityRedirectionConfig.ToLower() == "true";
+        }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_BeginRequest()
+        {
+            if (isHttpsRedirectionEnabled && !Context.Request.IsSecureConnection)
+                Response.Redirect(Context.Request.Url.ToString().Replace("http:", "https:"));
         }
     }
 }
