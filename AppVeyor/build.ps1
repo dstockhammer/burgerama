cc9e1e2c17d21819aa48e1a0be2395f120339bfc
Daniel Stockhammer <daniel@stockhammer.it>
2014-07-28 23:42:53 +0100

    Improve appveyor scripts

--- AppVeyor/build.ps1
+++ AppVeyor/build.ps1
@@ -1,5 +1,8 @@
 $solutionDir = ""
 $solutionName = ""
+$webProjDir = ""
+$webProjName = ""
+$artifactName = ""
 
 # it is required that project names start with Burgerama
 $projectNameParts = $env:APPVEYOR_PROJECT_NAME.split(".")
@@ -8,21 +11,32 @@ if ($projectNameParts[0] -ne "Burgerama") {
     Break
 }
 
-# Burgerama.Web.{Name?}
+# Burgerama.Web.{Name}
 if ($projectNameParts[1] -eq "Web") {
-    $solutionDir = "Web"
+    $solutionDir = "$env:APPVEYOR_BUILD_FOLDER\Web"
     $solutionName = "Burgerama.Web.sln"
+	$webProjDir = "$env:APPVEYOR_BUILD_FOLDER\Web\$projectNameParts[2]"
+	$webProjName = "$projectNameParts[2].csproj"
+	$artifactName = "$env:APPVEYOR_PROJECT_NAME.Api.zip"
 }
 # Burgerama.Services.{Name}
 ElseIf ($projectNameParts[1] -eq "Services") {
-    $project = $projectNameParts[2];
-    $solutionDir = "Services\$project"
-    $solutionName = "Burgerama.Services.$project.sln"
+    $solutionDir = "$env:APPVEYOR_BUILD_FOLDER\Services\$projectNameParts[2]"
+    $solutionName = "Burgerama.Services.$projectNameParts[2].sln"
+	$webProjDir = "$solutionDir\Api"
+	$webProjName = "Api.csproj"
+	$artifactName = "$env:APPVEYOR_PROJECT_NAME.zip"
 }
 Else {
     Add-AppveyorMessage "Invalid project name" -Category Error
     Break
 }
 
-nuget restore "$env:APPVEYOR_BUILD_FOLDER\$solutionDir\$solutionName"
-msbuild "$env:APPVEYOR_BUILD_FOLDER\$solutionDir\$solutionName" /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" /p:DeployOnBuild=True /p:PublishProfile=appveyor
+nuget restore "$solutionDir\$solutionName"
+
+Write-Host "Building $solutionName..."
+msbuild "$solutionDir\$solutionName" /verbosity:normal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" /p:Configuration="$env:CONFIGURATION" /p:DeployOnBuild=True /p:PublishProfile=appveyor
+
+Write-Host "Packaging $webProjName..."
+msbuild "$webProjDir\$webProjName" /t:Package /p:PackageLocation="$artifactName" /p:PackageAsSingleFile=True
+Push-AppveyorArtifact "$artifactName"
--- AppVeyor/pack_endpoint.ps1
+++ AppVeyor/pack_endpoint.ps1
@@ -12,8 +12,9 @@ if ($projectNameParts[1] -ne "Services") {
 
 $project = $projectNameParts[2];
 $projectDir = "$env:APPVEYOR_BUILD_FOLDER\Services\$project\Endpoint"
-$projectName = "Burgerama.Services.$project.Enpoint"
+$projectName = "Burgerama.Services.$project.Endpoint"
 
-msbuild "$env:APPVEYOR_BUILD_FOLDER\Configuration\Transform.proj" /target:"Transform" /property:"Configuration=$configuration;Path=$projectDir;Name=$projectName"
-7z a -tzip "$projectName.zip" "$projectDir\bin\$configuration\*"
+Write-Host "Transforming and packaging $projectName..."
+msbuild "$env:APPVEYOR_BUILD_FOLDER\Configuration\Transform.proj" /target:"Transform" /property:"Configuration=$env:CONFIGURATION;Path=$projectDir;Name=$projectName"
+7z a -tzip "$projectName.zip" "$projectDir\bin\$env:CONFIGURATION\*"
 Push-AppveyorArtifact "$projectName.zip"
